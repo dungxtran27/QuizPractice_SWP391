1ba397c97359520b995f83be219313d565a83741
minhthanh22032003 <minhthanh22032003@gmail.com>
2023-05-23 21:32:31 +0700

    them change pw servlet

--- src/java/DAO/userDAO.java
+++ src/java/DAO/userDAO.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
 import model.*;
 
 /**
@@ -89,5 +91,96 @@ public class userDAO extends MyDAO{
             e.printStackTrace();
         }
     }
-    
+        //cap nhat thong tin user trong db
+         public void editProfile(User us) {
+        Timestamp createdTime = new Timestamp(System.currentTimeMillis());
+        
+        xSql = "UPDATE [User] SET [fullname] =? , [phone] =?, [avatar]=?, [modify_date]= ?, [address] =? WHERE [userId] = ?";
+        try {
+            if (con != null) {
+                ps = con.prepareStatement(xSql);
+                ps.setString(1, us.getFullname());
+                ps.setString(2, String.valueOf(us.getPhone()));
+                ps.setString(3, us.getAvatar());
+                ps.setTimestamp(4, createdTime);
+                ps.setString(5, us.getAddress());
+                ps.setInt(6, us.getUserid());
+
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+         // tim user theo email va password
+          public User getUser(String email, String password) {
+        try {
+            if (con != null) {
+                xSql = "SELECT u.*, r.* FROM [User] u , [Role] r  WHERE [email] = ? AND [password] = ? and r.roleId = u.roleId";
+                ps = con.prepareStatement(xSql);
+                ps.setString(1, email);
+                ps.setString(2, password);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    User user = new User();
+                    Role role = new Role();
+                    
+                    role.setRole_id(rs.getInt("RoleID"));
+                    role.setRole_name(rs.getString("Name"));
+                    user.setUserid(rs.getInt("userId"));
+                    user.setFullname(rs.getString("fullname"));
+                    user.setPhone(rs.getInt("phone"));
+                    user.setEmail(rs.getString("email"));
+                    user.setAvatar(rs.getString("avatar"));
+                    user.setCreated_date(rs.getTimestamp("created_date"));
+                    user.setModify_date(rs.getTimestamp("modify_date"));
+                    user.setAddress(rs.getString("address"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    //tim nguoi dung theo id va cap nhat mat khau
+    public void changePassword(int userID, String newPassword) {
+        xSql = "UPDATE User SET password = ? WHERE userId = ?";
+        try {
+            if (con != null) {
+                ps = con.prepareStatement(xSql);
+                ps.setInt(2, userID);
+                ps.setString(1, newPassword);
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/java/model/user.java
+++ src/java/model/user.java
@@ -83,6 +83,118 @@ public class User {
     public void setPassword_token(String password_token) {
         this.password_token = password_token;
     }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(int roleid) {
+        this.roleid = roleid;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Timestamp created_date) {
+        this.created_date = created_date;
+    }
+
+    public Timestamp getModify_date() {
+        return modify_date;
+    }
+
+    public void setModify_date(Timestamp modify_date) {
+        this.modify_date = modify_date;
+    }
     
     
     
