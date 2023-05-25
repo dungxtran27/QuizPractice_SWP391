c50df0cd6fc05f5148c73af3344d2b70369df3cb
Trần Quang Dũng <dungtqhe170018@fpt.edu.vn>
2023-05-25 17:21:52 +0000

    Update DBContext.java

--- src/java/model/DBContext.java
+++ src/java/model/DBContext.java
@@ -28,7 +28,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "sa";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=animal";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_QPS04";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
