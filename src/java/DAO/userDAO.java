7d37e0dc7cad4fc6b1bb22ec7a359e5a20ce77d4
minhthanh22032003 <minhthanh22032003@gmail.com>
2023-05-28 16:11:03 +0700

    modify userDAO

--- src/java/DAO/userDAO.java
+++ src/java/DAO/userDAO.java
@@ -224,13 +224,13 @@ public class userDAO extends MyDAO {
                 int xuserid = rs.getInt(1);
                 String xusername = rs.getString(2);
                 String xpassword = rs.getString(3);
-                String xfullname = rs.getString(7);
-                String xphone = rs.getString(6);
-                String xaddress = rs.getString(8);
                 String xemail = rs.getString(5);
+                String xphone = rs.getString(6);
+                String xfullname = rs.getString(7);
+                String xaddress = rs.getString(8);
                 String xava = rs.getString(10);
 
-                x = new User(xuserid, xusername, xpassword, xfullname, xphone, xaddress, xemail, xava, role);
+                x = new User(xuserid, xusername, xpassword, xemail, xphone, xfullname, xaddress,  xava, role);
                 return x;
             }
 
